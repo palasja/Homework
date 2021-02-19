add38e379c7565e8feede47ddbad7bb44b4e0235
palasja <palasja@gmail.com>
2021-02-19 09:43:15 +0300

    Task9_1_4

---
+++ Task9_4/MyClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task9_4
+{
+    public class MyClass
+    {
+        [Test("Private", Success = true)]
+        private void PrivateMethod() 
+        {
+            Console.WriteLine("I private method");
+        }
+        [Test("Public", Success = true)]
+        public void PublicMecthod()
+        {
+            Console.WriteLine("I public methods");
+        }
+    }
+}
---
+++ Task9_4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+
+namespace Task9_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var methods = new MyClass().GetType().GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+            foreach (var method in methods)
+                foreach (var attr in method.GetCustomAttributes(true))
+                    if (attr.GetType() == typeof(TestAttribute)) 
+                    {
+                        Console.WriteLine(method.Name);
+                        var testAttribute = (TestAttribute)attr;
+                        Console.WriteLine($"{testAttribute.name} - {testAttribute.Success}");
+                    }
+        }
+    }
+}
