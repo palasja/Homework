593db565173bc6c66100f8a1fd0a28fa4f2c4bf6
palasja <palasja@gmail.com>
2021-02-25 17:30:13 +0300

    Task11-1

---
+++ Task11_1/BankAccount.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Task11_1
+{
+    class BankAccount
+    {
+        /*[ThreadStatic]*/
+        public static double _balance;
+        public static object locker = new object();
+
+        public double getBalance() { return _balance; }
+        public void Debet(object sum)
+        {
+            lock(locker) {
+                _balance += (double) sum;
+                Console.WriteLine($"sum = {sum}; balance = {_balance}");
+            }
+            Thread.Sleep(1000);
+        }
+
+        public void Credit(object obj)
+        {
+            double sum = (double)obj;
+            lock (locker)
+            {
+                if (_balance + sum >= 0)
+                {
+                    _balance += sum;
+                    Console.WriteLine($"sum = {sum}; balance = {_balance}");
+                }
+                else
+                {
+                    Console.WriteLine($"Can't {_balance} {sum}");
+                }
+            }
+        }
+    }
+}
---
+++ Task11_1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace Task11_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread[] threads = new Thread[10];
+            for (int i = 0; i < 10; i+=2)
+            {
+                int operation = i;
+                threads[i] = new Thread(() => new BankAccount().Debet((Double)operation));
+            }
+            for (int i = 1; i < 10; i+=2)
+            {
+                int operation = i;
+                threads[i] = new Thread(() => new BankAccount().Credit((Double)operation * (-1)));
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                threads[i].Start();
+            }
+            Console.ReadLine();
+        }
+    }
+}
+/*
+ 1. Создайте класс BankAccount с полем _balance и методами Credit и Debit для изменения баланса.
+Создайте несколько экземпляров класса в разных потоках и производите вызов методов Credit и Debit.
+Сделайте так чтоб баланс изменялся корректно. Весь по синхронизации между потоками должен находится внутри класса BankAccount.
+ */
